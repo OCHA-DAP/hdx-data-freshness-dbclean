498c34ec0970c9c5471dfb1ab58fa0d0412fac80
Mike <rans@un.org>
2023-05-01 15:36:43 +1200

    Structure like freshness emailer

--- src/hdx/freshness/dbactions/__main__.py
+++ src/hdx/freshness/dbactions/__main__.py
@@ -6,6 +6,7 @@ from os import getenv
 from typing import Optional
 
 from hdx.database import Database
+from hdx.database.dburi import get_params_from_connection_uri
 from hdx.utilities.dateparse import now_utc
 from hdx.utilities.dictandlist import args_to_dict
 from hdx.utilities.easy_logging import setup_logging
@@ -18,16 +19,17 @@ logger = logging.getLogger(__name__)
 
 
 def main(
-    db_url: Optional[str] = None,
+    db_uri: Optional[str] = None,
     db_params: Optional[str] = None,
     action: str = "clean",
 ) -> None:
-    """Run freshness database cleaner. Either a database connection string (db_url) or database
-    connection parameters (db_params) can be supplied. If neither is supplied, a local
-    SQLite database with filename "test_freshness.db" is assumed.
+    """Run freshness database cleaner. Either a database connection string
+    (db_uri) or database connection parameters (db_params) can be supplied. If
+    neither is supplied, a local SQLite database with filename "freshness.db"
+    is assumed.
 
     Args:
-        db_url (Optional[str]): Database connection string. Defaults to None.
+        db_uri (Optional[str]): Database connection URI. Defaults to None.
         db_params (Optional[str]): Database connection parameters. Defaults to None.
         action (bool): What action to take. "clone" to copy prod db for testing. Default is clean.
 
@@ -38,10 +40,10 @@ def main(
     logger.info(f"> Data freshness database clean 1.0")
     if db_params:  # Get freshness database server details
         params = args_to_dict(db_params)
-    elif db_url:
-        params = Database.get_params_from_sqlalchemy_url(db_url)
+    elif db_uri:
+        params = get_params_from_connection_uri(db_uri)
     else:
-        params = {"driver": "sqlite", "database": "test_freshness.db"}
+        params = {"dialect": "sqlite", "database": "freshness.db"}
     logger.info(f"> Database parameters: {params}")
     with Database(**params) as session:
         now = now_utc()
@@ -60,13 +62,13 @@ if __name__ == "__main__":
         description="Data Freshness Database Clean"
     )
     parser.add_argument(
-        "-db", "--db_url", default=None, help="Database connection string"
+        "-db", "--db_uri", default=None, help="Database connection string"
     )
     parser.add_argument(
         "-dp",
         "--db_params",
         default=None,
-        help="Database connection parameters. Overrides --db_url.",
+        help="Database connection parameters. Overrides --db_uri.",
     )
     parser.add_argument(
         "-a",
@@ -75,13 +77,13 @@ if __name__ == "__main__":
         help="Action to perform.",
     )
     args = parser.parse_args()
-    db_url = args.db_url
-    if db_url is None:
-        db_url = getenv("DB_URL")
-    if db_url and "://" not in db_url:
-        db_url = f"postgresql://{db_url}"
+    db_uri = args.db_uri
+    if db_uri is None:
+        db_uri = getenv("DB_URI")
+    if db_uri and "://" not in db_uri:
+        db_uri = f"postgresql://{db_uri}"
     main(
-        db_url=db_url,
+        db_uri=db_uri,
         db_params=args.db_params,
         action=args.action,
     )
